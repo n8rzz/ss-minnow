74d1ee4606d498a137e7bbd908b7f5b40731cbe8
Nate Geslin <ngeslin@p1u7o13.local>
2016-02-07 17:42:56 -0600

    Adds path pkg * Updates editorconfig * Adds more prompts * Removes nsp pkg and task

--- generator-ss-minnow/generators/app/index.js
+++ generator-ss-minnow/generators/app/index.js
@@ -2,6 +2,7 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var path = require('path');
 
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
@@ -9,18 +10,79 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the astonishing ' + chalk.red('generator-ss-minnow') + ' generator!'
+      'Welcome to the ' + chalk.red('generator-ss-minnow') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+      {
+          type: 'input',
+          name: 'title',
+          label: 'Name',
+          message: 'What is the name of your project?',
+          default: function() {
+              return path.basename(process.cwd());
+          }
+      },
+      {
+          type: 'input',
+          name: 'name',
+          label: 'Slug',
+          message: 'What would you like the slug to be?',
+          default: function(answers) {
+              var title = answers.title || 'client-project';
+
+              return title
+                  .toLowerCase()
+                  .replace(/[^a-z0-9]+/g, '-')
+                  .replace(/^\-+|\-+$/g, '');
+          }
+      },
+      {
+          type: 'input',
+          name: 'description',
+          label: 'Description',
+          message: 'One-line description',
+          default: ''
+      },
+      {
+          type: 'input',
+          name: 'version',
+          label: 'Version',
+          message: 'Version number (major.minor.patch)',
+          default: '1.0.0',
+          validate: function(answer) {
+              if (!(/^\d+\.\d+\.\d+([+\-]\S*)?$/).test(answer)) {
+                  return 'Must adhere to SemVer spec (major.minor.patch): http://semver.org/';
+              }
+
+              return true;
+          }
+      },
+      {
+          type: 'list',
+          name: 'jsFlavor',
+          label: 'Which flavor of Javascript?',
+          message: 'Javascript flavor',
+          choices: [
+          { name: 'es5',               value: 'es5' },
+          { name: 'es6',               value: 'es6' },
+          ]
+      },
+      {
+          type: 'checkbox',
+          name: 'additionalFeatures',
+          label: 'Features',
+          message: 'Additional Packages to include',
+          choices: [
+              { name: 'lodash',          value: 'lodash' },
+              { name: 'jquery',          value: 'jquery' }
+          ]
+      }
+    ];
 
     this.prompt(prompts, function (props) {
       this.props = props;
+      console.log('### PROMPTS ###: ', this.props);
       // To access props later use this.props.someOption;
 
       done();
@@ -34,7 +96,7 @@ module.exports = yeoman.generators.Base.extend({
     );
   },
 
-  install: function () {
-    this.installDependencies();
-  }
+  // install: function () {
+  //   this.installDependencies();
+  // }
 });
