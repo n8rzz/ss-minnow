a94076326f4824e97fc690488fb4070b43e63a7b
Nate Geslin <ngeslin@p1u7o13.local>
2016-02-12 23:01:05 -0600

    Adds assert helpers * Adds tests for assert helpers

---
+++ generator-ss-minnow/generators/app/templates/spec/util/assert.js
@@ -0,0 +1,98 @@
+import * as assert from '../../src/assets/scripts/util/assert.js';
+
+// const msg = 'An assertion message';
+let UNDEFINED;
+const DEFINED = 'defined';
+const STRING = 'string';
+const NUMBER = 33;
+const OBJECT = { k: 'val' };
+const ARRAY = [1, 2, 3, 4];
+
+
+describe('Assert.isNumber helper', () => {
+    it('THEN returns true if a number is passed', () => {
+        expect(
+            assert.isNumber(NUMBER)
+        ).toBe(true);
+    });
+
+    it('THEN returns true if NaN is passed', () => {
+        expect(
+            assert.isNumber(NaN)
+        ).toBe(true);
+    });
+
+    it('THEN returns false if a non-number is passed', () => {
+        expect(
+            assert.isNumber(STRING)
+        ).toBe(false);
+    });
+});
+
+describe('Assert.isDefined helper', () => {
+    it('THEN returns true if the property is defined', () => {
+        expect(
+            assert.isDefined(DEFINED)
+        ).toBe(true);
+    });
+
+    it('THEN returns false if the property is not defined', () => {
+        expect(
+            assert.isDefined(UNDEFINED)
+        ).toBe(false);
+    });
+});
+
+describe('Assert.isString helper', () => {
+    it('THEN returns true if a string is passed', () => {
+        expect(
+            assert.isString(STRING)
+        ).toBe(true);
+    });
+
+    it('THEN returns false if a non-string is passed', () => {
+        expect(
+            assert.isString(NUMBER)
+        ).toBe(false);
+    });
+});
+
+describe('Assert.isObject helper', () => {
+    it('THEN returns true if an object is passed', () => {
+        expect(
+            assert.isObject(OBJECT)
+        ).toBe(true);
+    });
+
+    it('THEN returns true if an array is passed', () => {
+        expect(
+            assert.isObject(ARRAY)
+        ).toBe(true);
+    });
+
+    it('THEN returns false if a non-object is passed', () => {
+        expect(
+            assert.isObject(STRING)
+        ).toBe(false);
+    });
+});
+
+describe('Assert.isArray helper', () => {
+    it('THEN returns true if an array is passed', () => {
+        expect(
+            assert.isArray(ARRAY)
+        ).toBe(true);
+    });
+
+    it('THEN returns true if an object is passed', () => {
+        expect(
+            assert.isArray(OBJECT)
+        ).toBe(false);
+    });
+
+    it('THEN returns false if a non-array is passed', () => {
+        expect(
+            assert.isArray(STRING)
+        ).toBe(false);
+    });
+});
---
+++ generator-ss-minnow/generators/app/templates/src/assets/scripts/util/assert.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2012-2015 Adam Ranfelt
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * trigger, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * assert utility
+ * @author Adam Ranfelt <adamRenny@gmail.com>
+ */
+export default function assert(condition, message) {
+    if (!condition) {
+        throw new Error(message);
+    }
+}
+
+export function assertParameter(condition, message) {
+    if (!condition) {
+        throw new TypeError('Error with parameter: ' + message);
+    }
+}
+
+export function isNumber(property) {
+    return typeof property === 'number';
+}
+
+export function isDefined(property) {
+    return typeof property !== 'undefined';
+}
+
+export function isString(property) {
+    return typeof property === 'string';
+}
+
+export function isObject(property) {
+    return typeof property === 'object';
+}
+
+export function isArray(object) {
+    return object instanceof Array;
+}
