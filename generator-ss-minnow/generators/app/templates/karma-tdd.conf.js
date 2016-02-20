8417337083e4363b5e4a36086e6117bbb7aff3d0
Nate Geslin <ngeslin@p1u7o13.local>
2016-02-19 22:59:54 -0600

    Bugfix-22 - Changes process order in karma.conf that fixes branch coverage inaccuracies * Updates some pkg versions as a result of bugfix * Adds mising kamra-tdd.conf.js file * Closes #22

---
+++ generator-ss-minnow/generators/app/templates/karma-tdd.conf.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = function(config) {
+    config.set({
+
+        basePath: '',
+
+        frameworks: ['jasmine', 'browserify'],
+
+        files: [
+            'src/**/*.js',
+            'spec/**/*.spec.js'
+        ],
+
+        browsers: ['PhantomJS'],
+
+        exclude: [],
+
+        reporters: ['spec'],
+
+        preprocessors: {
+            '**src/**/*.js': ['browserify'],
+            'spec/**/*.spec.js': ['browserify']
+        },
+
+        browserify: {
+            debug: true,
+            transform: [ 'babelify' ]
+        },
+
+        autoWatchBatchDelay: 250,
+
+        logLevel: 'warn',
+
+        specReporter: {
+            maxLogLines: 5,               // limit number of lines logged per test
+            suppressErrorSummary: true,   // do not print error summary
+            suppressFailed: false,        // do not print information about failed tests
+            suppressPassed: false,        // do not print information about passed tests
+            suppressSkipped: true         // do not print information about skipped tests
+        }
+    });
+};
