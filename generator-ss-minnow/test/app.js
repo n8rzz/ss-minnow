129aaedc828d0524e1c44f22b73fc174e8898cbc
n8rzz <teamtomkins23@gmail.com>
2016-02-24 07:58:15 -0600

    Adds more tests for generator * Adds pending tests for parts that are still missing * Adds more info to package.json

--- generator-ss-minnow/test/app.js
+++ generator-ss-minnow/test/app.js
@@ -1,9 +1,10 @@
 'use strict';
 var path = require('path');
-var assert = require('yeoman-assert');
-var helpers = require('yeoman-test');
+var fs = require('fs');
 var rimraf = require('rimraf');
 var temp = require('temp').track();
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
 
 describe('generator-ss-minnow:app', function () {
     var appPath = path.join(__dirname, '../generators/app');
@@ -14,80 +15,60 @@ describe('generator-ss-minnow:app', function () {
 
     after(function(done) {
         temp.cleanup();
-
         rimraf(path.join(__dirname, 'temp'), done);
     });
 
+    describe('Application generator files', function() {
+        before(function (done) {
+            helpers.run(require.resolve(appPath))
+                .withPrompts({
+                    title: 'Project Title',
+                    name: 'project-title',
+                    description: 'Description',
+                    version: '1.0.0'
+                })
+                .withOptions([
+                    { shouldUseBower: false, },
+                    { tcomb: true },
+                    { lodash: true }
+                ])
+                .on('end', done);
+        });
 
-    beforeEach(function (done) {
-        // helpers.testDirectory(path.join(__dirname, 'temp'), done);
+        it('generate application config files', function() {
+            assert.file([
+                '.babelrc',
+                '.eslintignore',
+                '.eslintrc',
+                '.gitignore',
+                'Gulpfile.js',
+                'karma-coverage.conf.js',
+                'karma-tdd.conf.js',
+                'karma.conf.js',
+                'sass-lint.yml'
+            ]);
+        });
 
-        helpers.run(require.resolve(appPath))
-            .withPrompts({
-                title: 'Project Title',
-                name: 'project-title',
-                description: 'Description',
-                version: '1.0.0'
-            })
-            .withOptions([
-                { shouldUseBower: false, },
-                { tcomb: true },
-                { lodash: true }
-            ])
-            .on('end', done);
-    });
+        it('should generate a package.json file', function() {
+            assert.file('package.json');
+        });
 
+        it('should have populated package.json correctly', function (done) {
+            fs.readFile('package.json', function (err, data) {
+                if (err) {
+                    done(err);
+                } else {
+                    var packageJson = JSON.parse(data);
+                    assert.equal(packageJson.name, 'project-title');
+                    assert.equal(packageJson.description, 'Description');
+                    assert.equal(packageJson.version, '1.0.0');
+                    done();
+                }
+            });
+        });
 
-
-    it('should generate a .babelrc file', function() {
-        assert.file('.babelrc');
-    });
-
-    // it('should generate a .bowerrc file', function() {
-    //     assert.file('.bowerrc');
-    // });
-
-    // it('should generate a .bowerrc file', function() {
-    //     assert.file('.bowerrc');
-    // });
-
-    // it('should generate an .editorconfig file', function() {
-    //     assert.file('editorconfig');
-    // });
-
-    it('should generate an .eslintignore file', function() {
-        assert.file('.eslintignore');
-    });
-
-    it('should generate an .eslintrc file', function() {
-        assert.file('.eslintrc');
-    });
-
-    it('should generate a .gitignore file', function() {
-        assert.file('.gitignore');
-    });
-
-    it('should generate a Gulpfile.js file', function() {
-        assert.file('Gulpfile.js');
-    });
-
-    it('should generate a karma-coverage.conf.js file', function() {
-        assert.file('karma-coverage.conf.js');
-    });
-
-    it('should generate a karma-tdd.conf.js file', function() {
-        assert.file('karma-tdd.conf.js');
-    });
-
-    it('should generate a karma.conf.js file', function() {
-        assert.file('karma.conf.js');
-    });
-
-    it('should generate a sass-lint.yml file', function() {
-        assert.file('sass-lint.yml');
-    });
-
-    it('should generate a package.json file', function() {
-        assert.file('package.json');
+        xit('should generate a `spec` directory', function() {});
+        xit('should generate a `src` directory', function() {});
+        xit('should generate a `tools` directory', function() {});
     });
 });
